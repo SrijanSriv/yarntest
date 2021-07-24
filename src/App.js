4c9fd6abd9998d9cc1c677c77162ec80c9041c2c
Srijan Srivastava <srijansrivastav01@gmail.com>
2021-07-24 18:02:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is just a test run to practice yarn adding and deployment. Check the Source Code README for more info maybe.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/SrijanSriv"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source Code
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
